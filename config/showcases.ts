d65eb8185ea6a1d27be724782e7fa0a5bce782a3
oneidealshop <167408083+oneidealshop@users.noreply.github.com>
2024-07-22 15:37:26 +0800

    Initial commit

---
+++ config/showcases.ts
@@ -0,0 +1,8 @@
+export const showcases = [
+  {
+    title: 'Landing Page Boilerplate',
+    description: 'A free, open-source, and powerful landing page boilerplate, ideal for various projects, enabling you to create a landing page in under an hour.',
+    websiteUrl: 'https://landingpage.weijunext.com',
+    og: 'https://landingpage.weijunext.com/og.png'
+  },
+]
